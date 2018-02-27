4ffa31feb8e5235fe4d812192366a41f2aee73ec
Rajeev <rajeevkumarsharma@outlook.com>
2018-02-27 08:42:03 +0530

    First Checkin

---
+++ src/demo/city/city.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+export interface City {
+    id: number;
+    name: string;
+    postCode: number;
+}
+
+@Injectable()
+export class CityService {
+    constructor(
+        private httpClient: HttpClient
+    ) { }
+
+    getCities(): Observable<City[]> {
+        const url = 'https://raw.githubusercontent.com/leovo2708/ngx-treeview/master/src/demo/city/cities.json';
+        return this.httpClient.get<City[]>(url);
+    }
+}
